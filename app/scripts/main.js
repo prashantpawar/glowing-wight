1bae351a9fd0fe2094cedd0bd44b7d62c618165a
Prashant Singh Pawar <prashantpawar@gmail.com>
2014-10-25 18:02:52 +0000

    Some synaptic code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
-console.log('\'Allo \'Allo!');
+var synaptic = require('synaptic'); // this line is not needed in the browser
+var Neuron = synaptic.Neuron,
+    Layer = synaptic.Layer,
+    Network = synaptic.Network,
+    Trainer = synaptic.Trainer,
+    Architect = synaptic.Architect;
