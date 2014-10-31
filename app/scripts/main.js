edc99c77dad9020638d6b7e9e9e3caea89fbb2ad
Prashant Singh Pawar <prashantpawar@gmail.com>
2014-10-31 23:59:47 +0000

    Scratched previous project and now going to use angular generated project

--- app/scripts/main.js
+++
@@ -1,9 +0,0 @@
-var Neuron = synaptic.Neuron,
-    Layer = synaptic.Layer,
-    Network = synaptic.Network,
-    Trainer = synaptic.Trainer,
-    Architect = synaptic.Architect;
-    
-window.onload = function () {
-    console.log("loaded");
-};
