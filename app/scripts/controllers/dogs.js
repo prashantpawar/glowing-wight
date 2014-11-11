ce2319063a036d2f4fe5f83ec7b7374ad9991ecf
Prashant Singh Pawar <prashantpawar@gmail.com>
2014-11-10 19:01:03 -0500

    -Fixed jshint errors

--- app/scripts/controllers/dogs.js
+++ app/scripts/controllers/dogs.js
@@ -8,8 +8,8 @@
  * Controller of the workspaceApp
  */
 angular.module('workspaceApp')
-  .controller('DogsCtrl', ['$scope', 'dogs', '$filter', function ($scope, dogs, $filter) {
-    dogs.loadJSON("data/dogs_by_breeds.json").then(function (a) {
-      $scope.breedData = a.data.results;
-    })
-  }]);
+.controller('DogsCtrl', ['$scope', 'dogs', function ($scope, dogs) {
+    dogs.loadJSON('data/dogs_by_breeds.json').then(function (a) {
+        $scope.breedData = a.data.results;
+    });
+}]);
